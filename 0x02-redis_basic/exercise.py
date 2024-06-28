cf39c54302a56c83efa84af09e0f982c2483a02b
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-06-28 07:35:21 +0300

    Retrieving lists

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,47 @@
 ''' redis application '''
 import redis
 import uuid
-import typing
+from typing import Callable, Optional, Union
+from functools import wraps
+
+
+def replay(method: Callable) -> None:
+    """Replay decorator"""
+    name = method.__qualname__
+    inputs = f"{name}:inputs"
+    outputs = f"{name}:outputs"
+    cache = method.__self__._redis
+    count = cache.get(name).decode('utf-8')
+    inputs_list = cache.lrange(inputs, 0, -1)
+    outputs_list = cache.lrange(outputs, 0, -1)
+    print(f"{name} was called {count} times:")
+    for i, o in zip(inputs_list, outputs_list):
+        print(f"{name}(*{i.decode('utf-8')}) -> {o.decode('utf-8')}")
+
+
+def count_calls(method: Callable) -> Callable:
+    """Count calls decorator"""
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """Wrapper function"""
+        self._redis.incr(method.__qualname__)
+        return method(self, *args, **kwargs)
+    return wrapper
+
+
+def call_history(method: Callable) -> Callable:
+    """Call history decorator"""
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """Wrapper function"""
+        name = method.__qualname__
+        inputs = f"{name}:inputs"
+        outputs = f"{name}:outputs"
+        self._redis.rpush(inputs, str(args))
+        data = method(self, *args, **kwargs)
+        self._redis.rpush(outputs, data)
+        return data
+    return wrapper
 
 
 class Cache():
@@ -11,13 +51,13 @@ class Cache():
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data: typing.Union[str, bytes, int, float]) -> str:
+    def store(self, data: Union[str, bytes, int, float]) -> str:
         """Store data in redis"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
 
-    def get(self, key: str, fn: typing.Optional[typing.Callable]):
+    def get(self, key: str, fn: Optional[Callable]):
         '''
             Retrieve data from Redis by key and
             optionally apply a conversion function.
