b5a29e1cb9b57d29308d9707b7d97d2fdfc93847
muhamedhaamdy <mmhamdyyy@gmail.com>
2024-06-28 06:56:47 +0300

    typing

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,6 +2,7 @@
 ''' redis application '''
 import redis
 import uuid
+import typing 
 
 
 class Cache():
@@ -10,7 +11,8 @@ class Cache():
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    def store(self, data):
-        id = str(uuid.uuid4())
-        self._redis.set(id, data)
-        return id
+    def store(self, data: typing.Union[str, bytes, int, float]) -> str:
+        """Store data in redis"""
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
